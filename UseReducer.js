44f6919e19b983da5100ec3130a4b1d4ea2aa42c
ManuM-5733 <manuvvp5@gmail.com>
2025-08-11 14:23:02 +0530

    Add files via upload

---
+++ UseReducer.js
@@ -0,0 +1,32 @@
+import React, { useReducer } from "react";
+
+const initialstate =0;
+const reducer = (state, action) => {
+    switch(action) {
+      case "add":
+          return state + 1;
+      case "subtract":
+          return state- 1;
+          case "reset":
+          return 0;
+      default:
+          throw new Error("Unexpected action");
+    }
+};
+
+const UseReducer = () => {
+
+  const [count, dispatch] = useReducer (reducer, initialstate);
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={() => dispatch("add")}>Add</button>
+      <button onClick={() => dispatch("subtract")}>Subtract</button>
+      <button onClick={() =>dispatch("reset")}>Reset</button>
+    </div>
+
+  );}
+
+
+export default UseReducer;
